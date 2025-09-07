c0a7390510df4365d6df03eb9fb5810c8cd2af15
Darshan vichhi <darshanvichhi111@gmail.com>
2025-09-07 09:34:31 +0530

    updated code

--- src/response/mod.rs
+++ src/response/mod.rs
@@ -66,77 +66,79 @@ impl Response {
     }
 }
 
-pub struct ResponseBuilder {
-    response: Response,
-}
+pub use builder::ResponseBuilder;
 
-impl ResponseBuilder {
-    pub fn new() -> Self {
-        Self {
-            response: Response::new(StatusCode::OK),
-        }
-    }
+// pub struct ResponseBuilder {
+//     response: Response,
+// }
 
-    pub fn status(mut self, status: StatusCode) -> Self {
-        self.response.status = status;
-        self
-    }
+// impl ResponseBuilder {
+//     pub fn new() -> Self {
+//         Self {
+//             response: Response::new(StatusCode::OK),
+//         }
+//     }
 
-    pub fn header<K, V>(mut self, key: K, value: V) -> Self
-    where
-        K: TryInto<HeaderName>,
-        V: TryInto<HeaderValue>,
-        K::Error: std::fmt::Debug,
-        V::Error: std::fmt::Debug,
-    {
-        if let (Ok(name), Ok(val)) = (key.try_into(), value.try_into()) {
-            self.response.headers.insert(name, val);
-        }
-        self
-    }
+//     pub fn status(mut self, status: StatusCode) -> Self {
+//         self.response.status = status;
+//         self
+//     }
 
-    pub fn json<T: Serialize>(mut self, data: &T) -> Result<Self> {
-        let body = serde_json::to_vec(data)?;
-        self.response.headers.insert(
-            HeaderName::from_static("content-type"),
-            HeaderValue::from_static("application/json"),
-        );
-        self.response.body = Bytes::from(body);
-        Ok(self)
-    }
+//     pub fn header<K, V>(mut self, key: K, value: V) -> Self
+//     where
+//         K: TryInto<HeaderName>,
+//         V: TryInto<HeaderValue>,
+//         K::Error: std::fmt::Debug,
+//         V::Error: std::fmt::Debug,
+//     {
+//         if let (Ok(name), Ok(val)) = (key.try_into(), value.try_into()) {
+//             self.response.headers.insert(name, val);
+//         }
+//         self
+//     }
 
-    pub fn text<T: Into<String>>(mut self, text: T) -> Self {
-        let text = text.into();
-        self.response.headers.insert(
-            HeaderName::from_static("content-type"),
-            HeaderValue::from_static("text/plain; charset=utf-8"),
-        );
-        self.response.body = Bytes::from(text);
-        self
-    }
+//     pub fn json<T: Serialize>(mut self, data: &T) -> Result<Self> {
+//         let body = serde_json::to_vec(data)?;
+//         self.response.headers.insert(
+//             HeaderName::from_static("content-type"),
+//             HeaderValue::from_static("application/json"),
+//         );
+//         self.response.body = Bytes::from(body);
+//         Ok(self)
+//     }
 
-    pub fn html<T: Into<String>>(mut self, html: T) -> Self {
-        let html = html.into();
-        self.response.headers.insert(
-            HeaderName::from_static("content-type"),
-            HeaderValue::from_static("text/html; charset=utf-8"),
-        );
-        self.response.body = Bytes::from(html);
-        self
-    }
+//     pub fn text<T: Into<String>>(mut self, text: T) -> Self {
+//         let text = text.into();
+//         self.response.headers.insert(
+//             HeaderName::from_static("content-type"),
+//             HeaderValue::from_static("text/plain; charset=utf-8"),
+//         );
+//         self.response.body = Bytes::from(text);
+//         self
+//     }
 
-    pub fn body<T: Into<Bytes>>(mut self, body: T) -> Self {
-        self.response.body = body.into();
-        self
-    }
+//     pub fn html<T: Into<String>>(mut self, html: T) -> Self {
+//         let html = html.into();
+//         self.response.headers.insert(
+//             HeaderName::from_static("content-type"),
+//             HeaderValue::from_static("text/html; charset=utf-8"),
+//         );
+//         self.response.body = Bytes::from(html);
+//         self
+//     }
 
-    pub fn build(self) -> Response {
-        self.response
-    }
-}
+//     pub fn body<T: Into<Bytes>>(mut self, body: T) -> Self {
+//         self.response.body = body.into();
+//         self
+//     }
 
-impl Default for ResponseBuilder {
-    fn default() -> Self {
-        Self::new()
-    }
-}
+//     pub fn build(self) -> Response {
+//         self.response
+//     }
+// }
+
+// impl Default for ResponseBuilder {
+//     fn default() -> Self {
+//         Self::new()
+//     }
+// }
